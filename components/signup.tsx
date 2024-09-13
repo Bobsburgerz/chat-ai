58afaa10a8222957ac0580b1129836c28261b709
BBLANDREW <drakerami599@gmail.com>
2024-09-13 05:43:23 +0000

    f

--- components/signup.tsx
+++ components/signup.tsx
@@ -3,8 +3,7 @@ import { useState } from 'react';
 import styles from './signup.module.css'; 
 import { useNewConvoMutation, useUpdateUserMutation } from '@/redux/services/appApi';
 import products from './products';
-
-
+import { signIn } from 'next-auth/react'; 
 type SignupProps = {
   onClose: () => void; 
   character?: string 
@@ -17,6 +16,10 @@ const Signup = ({ onClose , character }: SignupProps) => {
   const [newConvo ] = useNewConvoMutation();
   const [showModal, setShowModal] = useState(true);
   const [updateUser, { isError, isLoading }] = useUpdateUserMutation();
+  const handleGoogleSignup = () => {
+    signIn('google', { callbackUrl: '/' });
+  };
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
@@ -60,7 +63,7 @@ const Signup = ({ onClose , character }: SignupProps) => {
   return (
     <>
       <Head>
-        <title>Sign Up</title>
+        <title>Create An Account</title>
       </Head>
       <div className={styles.modalOverlay}>
         <div className={styles.modal}>
@@ -72,7 +75,7 @@ const Signup = ({ onClose , character }: SignupProps) => {
               className={styles.image}
             />
             <form onSubmit={handleSubmit} className={styles.form}>
-              <h1 className={styles.title}>Sign Up</h1>
+              <h1 className={styles.title}>Create An Account</h1>
               {error && <p className={styles.error}>{error}</p>}
               <label htmlFor="email" className={styles.label}>
                 Email
@@ -96,7 +99,15 @@ const Signup = ({ onClose , character }: SignupProps) => {
                 className={styles.input}
                 required
               />
-              <button type="submit" className={styles.button}>Sign Up</button>
+              <button type="submit" className={styles.button}>Start For Free</button>
+              <br/>
+              <div className={styles.or}><div className={styles.dash_r}/><div>or</div><div className={styles.dash}/></div>
+              <br/>
+              <div className={styles.oauthContainer}>
+            <button onClick={handleGoogleSignup} className={styles.googleButton}>
+              Google <img className={styles.googleIcon} src="https://res.cloudinary.com/dgyn6qakv/image/upload/v1726205352/Google__G__logo.svg_wbwbbz.png" />
+            </button>
+          </div>
             </form>
           </div>
           
