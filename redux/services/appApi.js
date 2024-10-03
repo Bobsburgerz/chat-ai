3cd2e271b7094dfadc474037b68eaa298c1c0ef0
Bobsburgerz <bobsburgers599@outlook.com>
2024-10-03 03:57:08 +0000

    U

--- redux/services/appApi.js
+++ redux/services/appApi.js
@@ -20,7 +20,7 @@ const baseQueryWithReauth = async (args, api, extraOptions) => {
 };
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: `https://ominous-trout-wrgv77796qrpcg67r-3000.app.github.dev/api`,
+  baseUrl: `https://cumcams.xyzapi`,
   credentials: 'include',
   prepareHeaders: (headers, {getState}) => {
  let token = 7
